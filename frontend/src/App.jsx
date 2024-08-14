b09d0b86dfa5a40df8090b9da93d2a159dc143a8
vasundharapandey <01fe21bcs245@kletech.ac.in>
2024-08-14 23:20:27 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='text-3xl  text-pink-600'>HI hello guys</div> 
+    </>
+  )
+}
+
+export default App
